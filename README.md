15031393b3f8ea75cd96cb8ad09cbf914bde2b71
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2019-05-18 11:04:55 +0200

    # ricerca
    Signed-off-by: EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
    
    # Programmi di Laboratorio di Programmazione in Linguaggio Python (PY). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+
 # ricerca
 
 # Programmi di Laboratorio di Programmazione in Linguaggio "Python" (PY). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
 
 
  - Programma "terzogradoforzabruta.py": Sia data l'equazione di terzo grado "x^3+4.5x^2+3.5x-3=0;"
-	Uno studio preliminare ha permesso di stabilire che esiste una soluzione nell'intervallo [0.3, 0.75]. Utilizzare un metodo a forza bruta per trovare tale soluzione.
+	Uno studio preliminare ha permesso di stabilire che esiste una soluzione nell'intervallo [0.3; 0.75]. Utilizzare un metodo a forza bruta per trovare tale soluzione.
+
+ - Programma "terzogradoforzabisezione.py": Sia data l'equazione di terzo grado "x^3+4.5x^2+3.5x-3=0;"
+	Uno studio preliminare ha permesso di stabilire che esiste una soluzione nell'intervallo [0.3; 0.75]. Utilizzare un metodo a bisezione per trovare tale soluzione.
--- terzogradobisezione.py
+++ terzogradobisezione.py
@@ -1,33 +1,39 @@
 
 '''
-
+	
+	
 	# ricerca
 
 	# Programmi di Laboratorio di Programmazione in Linguaggio "Python" (PY). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
 
 
-	 - Programma "terzogradoforzabruta.py": Sia data l'equazione di terzo grado "x^3+4.5x^2+3.5x-3=0;"
-		Uno studio preliminare ha permesso di stabilire che esiste una soluzione nell'intervallo [0.3; 0.75]. Utilizzo di un metodo a forza bruta per determinare tale soluzione.
+	 - Programma "terzogradoforzabisezione.py": Sia data l'equazione di terzo grado "x^3+4.5x^2+3.5x-3=0;"
+		Uno studio preliminare ha permesso di stabilire che esiste una soluzione nell'intervallo [0.3; 0.75]. Utilizzare un metodo a bisezione per trovare tale soluzione.
 
 '''
 
 print("Terzo Grado\n\n")
 print("Data l'equazione x^3+4.5x^2+3.5x-3=0;\n")
 print("Calcolo della soluzione compresa nell'intervallo (0.3; 0.75) mediante metodo a forza bruta\n\n\n\n")
-x=0.3
+xi=0.3
 xf=0.75
-varx=0.0001
 soglia=0.0001
 c=1
 print("x\n\n")
-print("Valore iniziale=",x,"\n")
+print("Valore iniziale=",xi,"\n")
 print("Valore finale=",xf,"\n")
-print("Passo:",varx,"\n")
 print("Valore soglia per lo zero=",soglia,"\n\n")
-while x<xf:
-	if abs(x**3.+4.5*x**2.+3.5*x-3.)<soglia:
+while xi<xf:
+	x=(xi+xf)/2
+	v=x**3+4.5*x**2+3.5*x-3
+	vxi=xi**3+4.5*xi**2+3.5*xi-3
+	vxf=xf**3+4.5*xf**2+3.5*xf-3
+	if abs(v)<soglia:
 		break
-	x+=varx
+	elif v*vxi>0:
+		xi=x
+	else:
+		xf=x
 	c+=1
 print("Soluzione: x=",x,"\n")
 print("Numero di passi=",c,"\n")
